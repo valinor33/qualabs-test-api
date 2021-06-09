1263b7c4e444718302f272f6392a5653f7d82bad
valinor <andresfullstack@gmail.com>
2021-06-08 21:55:36 -0300

    exercise made it!

--- index.js
+++ index.js
@@ -13,9 +13,22 @@ app.use(express.json());
 
 /* PROGRAM */
 
-let db = []
-
-/* PRIMER TEST -  */
+let db = {
+    'auth_module':
+    {
+        'authn.provider_1': [],
+        'authn.provider_2': [],
+        'authn.provider_3': [],
+        'authn.provider_4': []
+    },
+    'content_module':
+    {
+        'authz.provider_1': [],
+        'authz.provider_2': [],
+        'authz.provider_3': [],
+        'authz.provider_4': []
+    }
+}
 
 fs.readdir("./db", (err, files) => {
     if (err) console.log(err);
@@ -23,17 +36,112 @@ fs.readdir("./db", (err, files) => {
         files.forEach(file => {
             const selectedFile = `./db/${file}`
 
-            fs.readFile(selectedFile, JSON, (err, data) => {
-                if (err) console.log(err);
-                console.log(JSON.stringify(data));
-                db.push(JSON.stringify(data))
-            })
+            let rawdata = fs.readFileSync(selectedFile);
+            let user = JSON.parse(rawdata)
+
+            const content_module = user.provider.content_module;
+            const auth_module = user.provider.auth_module;
+
+            /* AUTH MODULE */
+            if (auth_module === "authz.provider_1") {
+
+                db.auth_module[auth_module].push(`./${file}`)
+
+            } else if (auth_module === "authz.provider_2") {
+
+                db.auth_module[auth_module].push(`./${file}`)
+
+            } else if (auth_module === "authz.provider_3") {
+
+                db.auth_module[auth_module].push(`./${file}`)
+
+            } else {
+
+                db.auth_module[auth_module].push(`./${file}`)
+            }
+
+            /* CONTENT MODULE */
+            if (content_module === "authz.provider_1") {
+
+                db.content_module[content_module].push(`./${file}`)
+
+            } else if (content_module === "authz.provider_2") {
+
+                db.content_module[content_module].push(`./${file}`)
+
+            } else if (content_module === "authz.provider_3") {
+
+                db.content_module[content_module].push(`./${file}`)
+                content_module
+            } else {
+
+                db.content_module[content_module].push(`./${file}`)
+            }
+
         })
     }
 })
 
-console.log(db);
+/* FILTERS */
+
+
+
+/* ROUTES */
+
+app.get("/api/users", (req, res) => {
+
+    res.status(200).json(db)
+})
+
 
 
 /* CONNECTION */
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
+//PRIMER TEST
+// let db = []
+
+// fs.readdir("./db", (err, files) => {
+//     if (err) console.log(err);
+//     else {
+//         files.forEach(file => {
+//             const selectedFile = `./db/${file}`
+
+//             let rawdata = fs.readFileSync(selectedFile);
+//             let user = JSON.parse(rawdata)   
+//             db.push(user)
+//         })
+//     }
+// })
+
+//en la llamada se agrega el sort para ordenar
+// db.sort(function (a, b) {
+//     return a - b;
+// });
+
+
+/* TEST 2 */
+// const readFile = () => fs.readdir("./db", (err, files) => {
+//     if (err) console.log(err);
+//     else {
+//         files.forEach(file => {
+//             const selectedFile = `./db/${file}`
+
+//             let rawdata = fs.readFileSync(selectedFile);
+//             let user = JSON.parse(rawdata)
+
+//             const content_module = user.provider.content_module;
+//             const auth_module = user.provider.auth_module;
+
+
+
+//             console.log(typeof (content_module));
+//             console.log(auth_module);
+
+
+//         })
+//     }
+//     return db
+// })
